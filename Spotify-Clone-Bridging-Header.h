b1eaf33f9e466d437cf7cb9fc4b70c9299b5705f
Azhar <azharcodes@gmail.com>
2020-04-03 17:45:05 +0530

    initial commit

---
+++ Spotify-Clone-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Spotify-Clone-Bridging-Header.h
+//  Spotify-Clone
+//
+//  Created by Azhar Anwar on 4/3/20.
+//  Copyright © 2020 Azhar Anwar. All rights reserved.
+//
+
+#ifndef Spotify_Clone_Bridging_Header_h
+#define Spotify_Clone_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+
+#endif /* Spotify_Clone_Bridging_Header_h */
---
+++ Spotify-Clone/SceneDelegate.swift
@@ -0,0 +1,74 @@
+//
+//  SceneDelegate.swift
+//  Spotify-Clone
+//
+//  Created by Azhar Anwar on 4/3/20.
+//  Copyright © 2020 Azhar Anwar. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    
+    var window: UIWindow?
+    lazy var rootViewController = ViewController()
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        if let _ = rootViewController.appRemote.connectionParameters.accessToken {
+            rootViewController.appRemote.connect()
+        }
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        if (rootViewController.appRemote.isConnected) {
+            rootViewController.appRemote.disconnect()
+        }
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+
+        let parameters = rootViewController.appRemote.authorizationParameters(from: url);
+
+        if let access_token = parameters?[SPTAppRemoteAccessTokenKey] {
+            rootViewController.appRemote.connectionParameters.accessToken = access_token
+            rootViewController.accessToken = access_token
+        } else if let _ = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            // Show the error
+        }
+    }
+    
+}
+
